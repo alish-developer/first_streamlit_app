1315ad774fb425902c5b467943a16c78fe64c87f
alish-developer <138883809+alish-developer@users.noreply.github.com>
2023-07-07 19:07:50 +0530

    Update streamlit_app.py
    added some details

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.taxt('Omega 3 & Blueberry Oatmeal')
+streamlit.taxt('Kale, Spinach & Rocket Smoothie')
+streamlit.taxt('Hard-Boiled Free-Range Egg')
